df0da8896d5efb8f9f472131f07b8e087ee8dbba
Arvind Jangid <jangidarvind843@gmail.com>
2026-05-05 21:22:57 +0530

    initial commit

---
+++ 4.Strings/string_methods.py
@@ -0,0 +1,7 @@
+course = 'Python For Beginners'
+print(len(course))# it tell the length of string
+print(course.upper())# output-PYTHON FOR BEGINNERS
+print(course.lower())# output-python for beginners
+print(course.find('P'))#it tells the index of character
+print(course.replace('Beginners',"Everyone"))#it replaces the given string with the new string
+print('Python' in course)#in checks the string present in variable.(True or False)
