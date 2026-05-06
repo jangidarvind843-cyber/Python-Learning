75106ebee28fd5085c0accb571150eebc7c5e4ed
Arvind Jangid <jangidarvind843@gmail.com>
2026-05-06 21:12:40 +0530

    initial commit

---
+++ 5.Arithmetic_Operator/Operator.py
@@ -0,0 +1,12 @@
+print(10+3) # Addition Operator
+print(10-3) #substract Operator
+print(10*3)# Multiplicaton Operator
+print(10/3)#division Operator
+print(10**3)# the 3 is power of 10 (10^3)
+print(10//3)#it gives integer value only
+print(10%3)# it gives remainder
+
+# Augmented assignment opertaor 
+x = 10
+x += 3
+print(x)
