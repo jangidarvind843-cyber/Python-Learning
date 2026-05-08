7eb52cb6006b856e56be26658bf770e877b6614a
Arvind Jangid <jangidarvind843@gmail.com>
2026-05-08 20:40:19 +0530

    initial Commit

---
+++ Example.py
@@ -0,0 +1,12 @@
+Hot = True
+Cold = False
+if Hot:
+    print("It's a Hot Day")
+    print("Drink plenty of Water")
+elif Cold:
+    print("It's a Cold Day")
+    print("Wear warm cloths")
+else:
+    print("It's a lovely Day")
+
+print("Enjoy Your day")
