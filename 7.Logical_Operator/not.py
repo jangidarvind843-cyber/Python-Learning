9dc2b50c7fcbd2791de15583f5006408d25c0ab4
Arvind Jangid <jangidarvind843@gmail.com>
2026-05-09 18:19:49 +0530

    Add files via upload

---
+++ 7.Logical_Operator/not.py
@@ -0,0 +1,8 @@
+Good_credit = True
+criminal_record = True
+if Good_credit and not criminal_record:
+    print("eligible for Loan")
+
+else:
+    print("Not eligible") 
+print("Have Good day")
---
+++ 7.Logical_Operator/operator.py
@@ -0,0 +1,14 @@
+# and - Operator
+High_income = True
+Good_credit_score = True
+
+if High_income and Good_credit_score:
+    print("Eligible for Loan")
+else:
+    print("Not eligible for Loan")
+
+#or operator
+if High_income or Good_credit_score:
+    print(" eligible for loan")
+else:
+    print("Not eligible for loan")
